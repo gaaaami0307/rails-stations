114676af84ffc4ab4d22632546ef265cbc2f10d6
Gami <s1320016@u-aizu.ac.jp>
2025-03-21 21:10:24 +0900

    fix:station03 errmsg

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -6,10 +6,24 @@ class Admin::MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
 
-    @movie.save
-    render json: @movie
+    if Movie.find_by(name: @movie.name).present?
+      flash.now[:alert] = "その名前の映画はすでに登録されています。" # 即時表示
+      render :new, status: :unprocessable_entity
+    elsif @movie.save
+      redirect_to admin_movies_path, notice: "登録に成功しました！"
+    else
+      flash.now[:alert] = "登録に失敗しました。"
+      render :new, status:400
+    end
   end
 
   def new
+    @movie = Movie.new
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url)
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,4 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-
-  def create
-    @movie = Movie.new(movie_params)
-
-    @movie.save
-    render json: @movie
-  end
 end
