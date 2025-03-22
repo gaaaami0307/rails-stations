59685106d95de0e93c854489839cde16f29f7731
Gami <s1320016@u-aizu.ac.jp>
2025-03-22 17:20:58 +0900

    station06 commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,18 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+
+    if !params[:is_showing].present?
+      params[:is_showing] = "all"
+    end
+
+    if params[:is_showing] != "all"
+      @movies = @movies.where(is_showing: params[:is_showing])
+    end
+
+    if params[:keyword].present?
+      query = "%#{params[:keyword]}%"
+      @movies = @movies.where("name LIKE ? OR description LIKE ?", query, query)
+    end
   end
 end
