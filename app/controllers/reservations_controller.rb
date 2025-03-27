38bce920a4b6cd887722b43be415410d687f4535
Gami <s1320016@u-aizu.ac.jp>
2025-03-28 04:01:13 +0900

    add:seat list page

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,11 @@
 class ReservationsController < ApplicationController
   def index
+    @movie = Movie.find_by(id: params[:movie_id])
+    @schedule = Schedule.find_by(id: params[:sid])
     @sheets = Sheet.all
+  end
+
+  def new
+    
   end
 end
