038bda96494b6fbdee5dd917a393755b1124ffdf
Gami <s1320016@u-aizu.ac.jp>
2025-03-28 04:30:45 +0900

    fix:redirect at null params

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,21 @@
 class ReservationsController < ApplicationController
   def index
     @movie = Movie.find_by(id: params[:movie_id])
-    @schedule = Schedule.find_by(id: params[:sid])
+    if !params[:schedule_id].present?
+      redirect_to movie_path(@movie), status:302
+    end
+
+    if !params[:date].present?
+      redirect_to movie_path(@movie), status:302
+    end
+
+    @schedule = Schedule.find_by(id: params[:schedule_id])
     @sheets = Sheet.all
   end
 
   def new
-    
+    @movie = Movie.find_by(id: params[:movie_id])
+    @schedule = Schedule.find_by(id: params[:schedule_id])
+    @sheets = Sheet.find_by(id: params[:sheet_id])
   end
 end
