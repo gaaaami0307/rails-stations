165904a768833922c1b0ba1dc5b80b523d8fe143
Gami <s1320016@u-aizu.ac.jp>
2025-03-30 17:50:48 +0900

    add: varidetion of email

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,14 @@
 class Reservation < ApplicationRecord
   belongs_to :schedule
   belongs_to :sheet
+
+  validate :valid_email_format
+
+  private
+
+  def valid_email_format
+    unless email =~ /^[\w+\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]+$/i
+      errors.add(:email, "は有効なメールアドレスではありません。")
+    end
+  end
 end
