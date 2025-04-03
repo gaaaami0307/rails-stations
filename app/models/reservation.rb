88e93c3ac2c7e864473ce8b9abd59db077f0e559
Gami <s1320016@u-aizu.ac.jp>
2025-04-04 01:44:08 +0900

    fix error 1

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :schedule
   belongs_to :sheet
-
+  validates :date, :schedule_id, :sheet_id, uniqueness: { scope: [:schedule_id, :sheet_id, :date], message: "この組み合わせはすでに予約されています。" }
   validate :valid_email_format
 
   private
