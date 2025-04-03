65330868a3013bf4e56d523298f36b964cb961b8
Gami <s1320016@u-aizu.ac.jp>
2025-04-04 01:33:26 +0900

    add:delete reservation

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -6,28 +6,6 @@ class Admin::ReservationsController < ApplicationController
   end
 
   def new
-
-    @movie = Movie.find_by(id: params[:movie_id])
-
-    if !params[:sheet_id].present?
-      redirect_to movie_path(@movie), status:302
-      return
-    end
-
-    if !params[:date].present?
-      redirect_to movie_path(@movie), status:302
-      return
-    end
-
-    @schedule = Schedule.find_by(id: params[:schedule_id])
-    @sheet = Sheet.find_by(id: params[:sheet_id])
-
-    if Reservation.find_by(schedule_id: @schedule.id, sheet_id: @sheet.id).present?
-      flash[:alert]="その座席は予約済みです。"
-      redirect_to movie_path(@movie), status:302
-      return
-    end
-
     @reservation = Reservation.new
   end
 
@@ -38,14 +16,54 @@ class Admin::ReservationsController < ApplicationController
 
     if Reservation.find_by(date: @reservation.date, sheet_id: @reservation.sheet_id, schedule_id: @reservation.schedule_id) != nil
       flash[:alert] = "その座席はすでに予約済みです" # 即時表示
-      redirect_to movie_reservation_path(@movie, schedule_id: @schedule.id, date: @reservation.date)
+      redirect_to admin_reservations_path
+      return
     elsif @reservation.save
-      redirect_to movie_path(@movie), notice: "予約が完了しました"
+      redirect_to admin_reservations_path, notice: "予約が完了しました"
     else
       flash[:alert] = "予約に失敗しました。"
-      redirect_to movie_reservation_path(@movie, schedule_id: @schedule.id, date: @reservation.date)
+      redirect_to admin_reservations_path
     end
   end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+    @schedule = @reservation.schedule
+    @movie = @schedule.movie
+    @sheet = @reservation.sheet
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+    @schedule = @reservation.schedule
+    @movie = @schedule.movie
+    @sheet = @reservation.sheet
+
+    if Reservation.find_by(date: params[:reservation][:date], sheet_id: params[:reservation][:sheet_id], schedule_id: params[:reservation][:schedule_id]) != nil
+      flash.now[:alert] = "その座席はすでに予約済みです" # 即時表示
+      render :show, status:400
+    elsif @reservation.update(reservation_params)
+      redirect_to admin_reservations_path, notice: "更新に成功しました！"
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :show, status:400
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @schedule = @reservation.schedule
+    @movie = @schedule.movie
+    @sheet = @reservation.sheet
+
+  if @reservation.delete
+      redirect_to admin_reservations_path, notice: "削除に成功しました！"
+    else
+      flash.now[:alert] = "削除に失敗しました。"
+      render :show, status:400
+    end
+  end
+
   private
 
   def reservation_params
